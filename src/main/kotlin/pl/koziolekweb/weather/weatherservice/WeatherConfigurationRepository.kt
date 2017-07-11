77fd3fd9bb7e5fbb408415d2cf40e0a42fce3600
Bartek Kuczynski <BKuczynski@luxoft.com>
2017-07-11 08:07:52 +0200

    new Version

---
+++ src/main/kotlin/pl/koziolekweb/weather/weatherservice/WeatherConfigurationRepository.kt
@@ -0,0 +1,15 @@
+package pl.koziolekweb.weather.weatherservice
+
+import org.springframework.stereotype.Component
+
+/**
+ * Created by BKuczynski on 2017-07-06.
+ */
+@Component
+open class WeatherConfigurationRepository {
+
+
+}
+
+val SERVICE_PREFIX = "WS"
+val STORAGE_PREFIX = "STR"
